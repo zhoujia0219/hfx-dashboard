5fd72a4e71f80e2c0b3afec599d5b62603062aab
shijuanli <juanli.shi@ruipos.com>
2021-08-11 18:01:49 +0800

    框架结构变更测试

--- apps/app_sales_bymonth_index.py
+++ apps/app_sales_bymonth_index.py
@@ -1,10 +1,11 @@
 import dash
+from flask_login import login_required
 
 from conf.hfx_dashboard import BOOTSTRAP_THEME
 from conf.router_conts import URL_SALES_BYMONTH
 
 
-def register_dash_app(app):
+def register_sales_app(app):
     from apps.layouts.app_sales_bymonth_layout import layout
     from apps.callbacks.app_sales_by_month_callbacks import register_callbacks
 
@@ -18,9 +19,17 @@ def register_dash_app(app):
                          suppress_callback_exceptions=True,
                          url_base_pathname=URL_SALES_BYMONTH,
                          meta_tags=[meta_viewport],
-                         external_stylesheets=[BOOTSTRAP_THEME])
+                         external_stylesheets=[BOOTSTRAP_THEME],
+                         # index_string=base_html_string
+                         )
 
     with app.app_context():
         dash_app.title = 'Dash Example'
         dash_app.layout = layout
         register_callbacks(dash_app)
+
+
+def _protect_dash_views(dash_app):
+    for view_func in dash_app.server.view_functions:
+        if view_func.startswith(dash_app.config.url_base_pathname):
+            dash_app.server.view_functions[view_func] = login_required(dash_app.server.view_functions[view_func])
--- flask_app.py
+++ flask_app.py
@@ -1,9 +1,10 @@
 from importlib import import_module
+from os import path
 
-from flask import Flask
+from flask import Flask, url_for
 from flask_caching import Cache
 
-from apps.app_sales_bymonth_index import register_dash_app
+from apps.app_sales_bymonth_index import register_sales_app
 from conf import hfx_dashboard
 
 # #########################
@@ -27,13 +28,44 @@ def register_blueprints(app):
     app.register_blueprint(module.blueprint)
 
 
+def apply_themes(app):
+    """
+    Add support for themes.
+
+    If DEFAULT_THEME is set then all calls to
+      url_for('static', filename='')
+      will modfify the url to include the theme name
+
+    The theme parameter can be set directly in url_for as well:
+      ex. url_for('static', filename='', theme='')
+
+    If the file cannot be found in the /static/<theme>/ location then
+      the url will not be modified and the file is expected to be
+      in the default /static/ location
+    """
+
+    @app.context_processor
+    def override_url_for():
+        return dict(url_for=_generate_url_for_theme)
+
+    def _generate_url_for_theme(endpoint, **values):
+        if endpoint.endswith('static'):
+            themename = values.get('theme', None) or \
+                        app.config.get('DEFAULT_THEME', None)
+            if themename:
+                theme_file = "{}/{}".format(themename, values.get('filename', ''))
+                if path.isfile(path.join(app.static_folder, theme_file)):
+                    values['filename'] = theme_file
+        return url_for(endpoint, **values)
+
+
 def create_app(config, selenium=False):
     app = Flask(__name__, static_folder='base/static')
     app.config.from_object(config)
     if selenium:
         app.config['LOGIN_DISABLED'] = True
 
-    register_dash_app(app)
+    register_sales_app(app)
     register_extensions(app)
     register_blueprints(app)
 
