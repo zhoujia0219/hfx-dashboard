e49eda6f7e1ac8763ec5e58722ac0606af62d515
罗芳 <9511802+luo-fang99@user.noreply.gitee.com>
2021-10-26 14:18:04 +0800

    上传蔡大胖后半部分动态页面

--- apps/layouts/app_caidapang_data_layout.py
+++ apps/layouts/app_caidapang_data_layout.py
@@ -3,15 +3,11 @@ import dash_table
 import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_html_components as html
-
+from services.srv_cdp_importData import data_month_order, data_time_allstore_avg, data_time_storecount, \
+    data_sales_proport,res
 import plotly.graph_objects as go
-import plotly.figure_factory as ff
-
+import plotly.express as px
 from apps.layouts import app_cdp_layout
-
-###############
-# sidebar
-###############
 
 # 侧边栏
 sidebar = html.Div(
@@ -19,149 +15,151 @@ sidebar = html.Div(
     children=[
         html.H4(children="门店月度销售分析"),
         html.Hr(),
-        # html.P(children="可通过条件筛选过滤数据，也可以改变维度和图形样式", className="small", style={'color': 'gray'}),
-        # html.Div([
-        #     filter_date_range.filter_month_range(date_range, start_month, stop_month),
-        #     filter_city_level.filter_city_level(),
-        #     filter_channels.filter_channels(),
-        #     filter_store_age.filter_store_age(),
-        #     filter_store_area.filter_store_area(),
-        #     filter_store_star.filter_store_star(),
-        #     dbc.Button(children='重新计算', id='f_submit', color="primary", className="mt-3", block=True),
-        # ]),
     ],
 )
 
 
 # 定义方法： 门店销售时段&平均销售额
 def fig_time_bar_method():
-    date = ['11:00-12:00', '12:00-13:00', '13:00-14:00', '14:00-15:00', '15:00-16:00', '16:00-17:00', '17:00-18:00',
-            '18:00-19:00', '19:00-20:00', '20:00-21:00', '21:00-22:00']
-    values = [104, 131, 107, 154, 220, 240, 259, 279, 246, 233, 219]
-    fig = go.Figure(data=[go.Bar(
+    date = data_time_allstore_avg['1_x']
+    values = data_time_allstore_avg['1_y']
+    fig = px.bar(
         x=date,
         y=values,
-        name='门店销售时段*平均销售额',
         text=values,
-        textposition='outside',
-        textfont_size=12,
-        #marker_color='rgb(55, 83, 109)'
-    )])
-    fig.update_layout(
-        #xaxis_tickangle=-45,
+        title='门店销售时段●平均销售额',
         template='plotly_white',
-        # 柱状图模式
-        barmode='group',
-        # 组间距离
-        bargap=0.5,
-        # 组内距离
-        bargroupgap=0.2,
-        font=dict(family='arial',
-                  color='#000000',
-                  size=12),
-        # 显示文本/数值
-        uniformtext_mode='show',
-        # 文本/数值显示的大小
-        uniformtext_minsize=8,
-        title_text='门店销售时段*平均销售额',
-        # margin=dict(t=5, l=5, b=5, r=5)
     )
+    fig.update_layout(margin=dict(l=20, r=0, t=40, b=20), title_x=0.5)
+    fig.update_traces(texttemplate='%{text:.0f}', textposition='outside', )
+    fig.update_xaxes(title=None)
+    fig.update_yaxes(title=None)
+    return fig
+    # fig = go.Figure(data=[go.Bar(
+    #     x=date,
+    #     y=values,
+    #     name='门店销售时段*平均销售额',
+    #     text=values,
+    #     textposition='outside',
+    #     textfont_size=12,
+    #     #marker_color='rgb(55, 83, 109)'
+    # )])
+    # fig.update_layout(
+    #     #xaxis_tickangle=-45,
+    #     template='plotly_white',
+    #     # 柱状图模式
+    #     barmode='group',
+    #     # 组间距离
+    #     bargap=0.5,
+    #     # 组内距离
+    #     bargroupgap=0.2,
+    #     font=dict(family='arial',
+    #               color='#000000',
+    #               size=12),
+    #     # 显示文本/数值
+    #     uniformtext_mode='show',
+    #     # 文本/数值显示的大小
+    #     uniformtext_minsize=8,
+    #     title_text='门店销售时段*平均销售额',
+    #     # margin=dict(t=5, l=5, b=5, r=5)
+    # )
     # fig.update_traces(hoverinfo='label+percent',    # 设置鼠标悬浮上去时显示的文本
     #                   textfont_size=20,
     #                  )
-    return fig
 
 
 # 定义方法： （13家）日均时段销售额
 def fig_sales_bar_method():
-    date = ['00:00-01:00', '01:00-02:00', '02:00-03:00', '03:00-04:00', '04:00-05:00', '05:00-06:00', '06:00-07:00',
-            '07:00-08:00', '08:00-09:00', '09:00-10:00', '10:00-11:00', '11:00-12:00', '12:00-13:00', '13:00-14:00',
-            '14:00-15:00', '15:00-16:00', '16:00-17:00', '17:00-18:00', '18:00-19:00', '19:00-20:00', '20:00-21:00',
-            '21:00-22:00', '22:00-23:00', '23:00-00:00']
-    values = [48.79, 20.56, 11.04, 0.31, 0.15, 6.55, 9.44, 0.03, 0.09, 1.37, 30.65, 147.23, 191.06, 157.79, 217.31,
-              315.91, 367.04, 400.01, 424.90, 405.58, 381.62, 347.57, 252.39, 143.21]
-    fig = go.Figure(data=[go.Bar(
+    date = data_month_order['1_x']
+    values = data_month_order['1_y']
+    fig = px.bar(
         x=date,
         y=values,
-        name='kkk',
         text=values,
-        textposition='outside',
-        textfont_size=25
-        # marker_color='rgb(55, 83, 109)'
-    )])
-    fig.update_layout(
-        # X坐标轴标签倾斜度数
-        xaxis_tickangle=-45,
-        # 背景颜色
+        title='营业额&利润排名重合门店(13家)\n日均时段销售额',
         template='plotly_white',
-        # 柱状图模式
-        barmode='group',
-        # 组间距离
-        bargap=0.2,
-        # 组内距离
-        bargroupgap=0.2,
-        # 字体样式、颜色、大小设置
-        font=dict(family='arial',
-                  color='#000000',
-                  size=12),
-        # 显示文本/数值
-        uniformtext_mode='show',
-        # 文本/数值显示的大小
-        uniformtext_minsize=11,
-        # margin=dict(t=5, l=5, b=5, r=5)
     )
+    fig.update_layout(margin=dict(l=20, r=0, t=40, b=20), title_x=0.5, barmode='group', bargap=0.2, bargroupgap=0.2,
+                      xaxis_tickangle=-45, )
+    fig.update_traces(texttemplate='%{text:.0f}', textposition='outside', )
+    fig.update_xaxes(title=None)
+    fig.update_yaxes(title=None)
+    # fig.update_layout(
+    #     # X坐标轴标签倾斜度数
+    #     xaxis_tickangle=-45,
+    #     # 柱状图模式
+    #     barmode='group',
+    #     # 组间距离
+    #     bargap=0.2,
+    #     # 组内距离
+    #     bargroupgap=0.2,
+    #     # 字体样式、颜色、大小设置
+    #     font=dict(family='arial',
+    #               color='#000000',
+    #               size=12),
+    #     title_x=0.5
+    #     # margin=dict(t=5, l=5, b=5, r=5)
+    # )
+    # fig.update_traces(texttemplate='%{text:.0f}', textposition='outside', )
     # fig.update_traces(hoverinfo='label+percent',    # 设置鼠标悬浮上去时显示的文本
     #                   textfont_size=20,
     #                  )
     return fig
 
 # 时段门店数量数据
-df_stores_count= pd.DataFrame({
-    '时段①': ['00:00-01:','01:00-02:00','02:00-03:00','03:00-04:00','04:00-05:00','05:00-06:00','06:00-07:00','07:00-08:00'],
-    '门店数①': [65,26,19,7,9,8,10,20],
-    '时段②': ['08:00-09:00','09:00-10:00','10:00-11:00','11:00-12:00','12:00-13:00','13:00-14:00','14:00-15:00','15:00-16:00'],
-    '门店数②': [137,161,166,166,166,166,166,166],
-    '时段③': ['16:00-17:00','14:00-15:00','15:00-16:00','16:00-17:00','17:00-18:00','18:00-19:00','19:00-20:00','20:00-21:00'],
-    '门店数③': [166,166,166,166,166,166,166,166],
-    '时段④': ['21:00-22:00','22:00-23:00','23:00-00:00','','','','',''],
-    '门点数④': [166,159,119,'','','','',''],
+df_stores_count = pd.DataFrame({
+    '时段①': res[0],
+    '门店数①': res[1],
+    '时段②': res[2],
+    '门店数②': res[3],
+    '时段③': res[4],
+    '门店数③': res[5],
+    '时段④': res[6],
+    '门点数④': res[7],
 })
+
 # 时段门店数量table
-table_stores_count= dash_table.DataTable(
+table_stores_count = dash_table.DataTable(
     id='table_stores',
     data=df_stores_count.to_dict('records'),
     columns=[{'name': i, 'id': i} for i in df_stores_count.columns],
 )
 
 # 销售占比数据
-df_sales_tot = pd.DataFrame({
-    '编号': [1,2,3,4,5,6,7,8,9,10,11,12,13,''],
-    '门店名': ['蔡大胖炸洋芋（华商店）','蔡大胖炸洋芋（火炮街店）','蔡大胖炸洋芋（拉萨金马店）','蔡大胖炸洋芋（华熙528店）','蔡大胖炸洋芋（中央华城店）',
-                       '蔡大胖炸洋芋（南充高坪中学店）','蔡大胖炸洋芋（川师成龙校区店）','蔡大胖炸洋芋（九里晴川店）','蔡大胖炸洋芋（滨江和城店）','蔡大胖炸洋芋（花果园店）',
-                        '蔡大胖炸洋芋（天府逸家店）','蔡大胖炸洋芋（华润时光里店）','蔡大胖炸洋芋（广安北街店）','平均占比'],
-    '堂食占比': [0.8,0.5,0.2,0.4,0.3,0.4,0.4,0.3,0.5,0.3,0.3,0.3,0.3,0.4],
-    '外卖占比': [0.2,0.5,0.8,0.6,0.7,0.6,0.6,0.7,0.5,0.7,0.7,0.7,0.7,0.6],
-    '营业额排名':[1,8,12,9,11,5,2,6,19,3,13,10,18," "],
-    '净利润排名': [1,2,3,4,5,6,7,8,10,12,13,14,20," "]
+data_sales_proport = pd.DataFrame({
+    '编号': data_sales_proport[4],
+    '门店名': data_sales_proport[0],
+    '堂食占比': data_sales_proport['1_y'],
+    '外卖占比': data_sales_proport['2_y'],
+    '营业额排名': data_sales_proport[3],
+    '净利润排名': data_sales_proport[4]
 })
+
 # 销售占比table
 table_sales_tot = dash_table.DataTable(
     id='table_sales',
-    data=df_sales_tot.to_dict('records'),
-    columns=[{'name': i, 'id': i} for i in df_sales_tot.columns],
+    data=data_sales_proport.to_dict('records'),
+    columns=[{'name': i, 'id': i} for i in data_sales_proport.columns],
     style_data_conditional=[
         {
             'if': {
                 'column_id': '堂食占比',
                 'filter_query': '{堂食占比} >= 0.4',
-                },
+            },
             'backgroundColor': 'pink'
+        },
+        {
+            'if': {
+                'colum_id': '堂食占比',
+                'filter_query': '{堂食占比[-1]}'
+            },
+            'backgroundColor': 'white'
         },
         {
             'if': {
                 'column_id': '外卖占比',
                 'filter_query': '{外卖占比} > 0.6',
-                },
+            },
             'backgroundColor': 'yellow'
         },
     ]
@@ -171,42 +169,24 @@ table_sales_tot = dash_table.DataTable(
 module_ave_sales_bar = dbc.Card(
     children=dbc.CardBody(
         children=[
-            dbc.Row([
-                dbc.Col(
-                    children=[
-                        # 用户选项
-                        html.Div(
-                            [
-                                html.H5(
-                                    id='time_bar',
-                                    children=' ',
-                                    className='media-body'
+            html.Div(
+                children=[
+                    dcc.Loading(id='time_bar_loading',
+                                type='circle',
+                                children=[
+                                    dcc.Graph(figure=fig_time_bar_method(),
+                                              config={
+                                                  # 隐藏浮动工具栏
+                                                  'displayModeBar': False
+                                              }
+                                              )
+                                ],
+                                style={'width': 120}
                                 ),
-                                html.Hr(),
-                                # 图
-                                html.Div(
-                                    children=[
-                                        dcc.Loading(id='time_bar_loading',
-                                                    type='circle',
-                                                    children=[
-                                                        dcc.Graph(figure=fig_time_bar_method(),
-                                                                  config={
-                                                                        #隐藏浮动工具栏
-                                                                        'displayModeBar': False
-                                                                  }
-                                                                  )
-                                                    ],
-                                                    style={'width': 120}
-                                                    ),
-                                    ],
-                                    #设置固定高度，增加页面体验效果
-                                    style={"height":"450px"}
-                                )
-                            ]
-                        )
-                    ]
-                )
-            ])
+                ],
+                # 设置固定高度，增加页面体验效果
+                style={"height": "450px"}
+            )
         ]
     )
 )
@@ -215,33 +195,23 @@ module_ave_sales_bar = dbc.Card(
 module_time_sales_bar = dbc.Card(
     children=dbc.CardBody(
         children=[
-            # 用户选项
+            # 图
             html.Div(
-                [
-                    html.H5(
-                        id='sales_bar',
-                        children=' ',
-                        className='media-body'
-                    ),
-                    # 图
-                    html.Div(
-                        children=[
-                            dcc.Loading(id='sales_bar_loading',
-                                        type='circle',
-                                        children=[
-                                            dcc.Graph(figure=fig_sales_bar_method(),
-                                                      config={
-                                                            #隐藏浮动工具栏
-                                                            'displayModeBar': False
-                                                      }
-                                                      )
-                                        ],
-                                        style={'width': 120}
-                                        ),
-                        ],
-                        style={"height":"450px"}
-                    )
-                ]
+                children=[
+                    dcc.Loading(id='sales_bar_loading',
+                                type='circle',
+                                children=[
+                                    dcc.Graph(figure=fig_sales_bar_method(),
+                                              config={
+                                                  # 隐藏浮动工具栏
+                                                  'displayModeBar': False
+                                              }
+                                              )
+                                ],
+                                style={'width': 120}
+                                ),
+                ],
+                style={"height": "450px"}
             )
         ]
     )
@@ -266,7 +236,7 @@ module_sales_tot_table = dbc.Card(
         ]
     )
 )
-#销售时间段统计（门店数量）tbale
+# 销售时间段统计（门店数量）tbale
 module_stores_count_table = dbc.Card(
     children=dbc.CardBody(
         children=[
---
+++ services/srv_cdp_importData.py
@@ -0,0 +1,178 @@
+import math
+
+from utils.clickhouse_conn import clickHouseConn
+from pandas.core.frame import DataFrame
+
+client = clickHouseConn()  # 建立导入类的对象
+
+
+# 时间段方法
+def switchStr(minsrt, maxstr):
+    if (len(str(minsrt)) == 1):
+        minsrt = '0{}:00'.format(minsrt)
+    else:
+        minsrt = '{}:00'.format(minsrt)
+    if (len(str(maxstr)) == 1):
+        maxstr = '0{}:00'.format(maxstr)
+    else:
+        maxstr = '{}:00'.format(maxstr)
+    return minsrt + "-" + maxstr
+
+
+if __name__ == '__main__':
+    switchStr(1, 2)
+# 取一天内的时间段
+sql_time_order = """
+    select time_period_code,min_value,max_value from cdp.cdim_time_period
+"""
+query_time_order = client.query_sql(sql_time_order)
+print(query_time_order)
+time_order = []
+for row in query_time_order:
+    time_order.append([row[0], switchStr(row[1], row[2])])
+
+# 取计算门店数量的数据
+sql_store_count = """
+    select time_period.time_period_code,count(time_period.quantity)/2 
+    from cdp.sd_trade_byday array join time_period 
+    group by `time_period.time_period_code` 
+    having `time_period.quantity` !='0' 
+    order by `time_period.time_period_code`;
+"""
+query_store_count = client.query_sql(sql_store_count)
+
+# 取所有门店都在营业的时间段（11点-21点间）的平均销售额
+sql_all_store_avg = """
+    select time_period.time_period_code,AVG(time_period.amount) 
+    from cdp.sd_trade_byday array join time_period 
+    group by `time_period.time_period_code` 
+    having `time_period.time_period_code` BETWEEN '2021090111' AND '2021090121' 
+    order by `time_period.time_period_code`; 
+"""
+query_all_store_avg = client.query_sql(sql_all_store_avg)
+
+# 取营业额和利润排名前20且重合的门店在一天的时段内的平均销售额
+sql_order_data = """
+    select store_name,sum(trade_amount_avg),sum(net_profit_total) 
+    from cdp.sd_trade_bymonth 
+    group by store_name 
+    order by sum(trade_amount_avg) DESC ,sum(net_profit_total) DESC LIMIT 20
+"""
+query_order_data = client.query_sql(sql_order_data)
+
+sql_store_merge = """
+    select store_name,store_code
+    from cdp.bo_store
+"""
+query_store_merge = client.query_sql(sql_store_merge)
+
+sql_order_store_avg = """
+    select time_period.time_period_code,AVG(time_period.amount) 
+    from cdp.sd_trade_byday array join time_period 
+    WHERE store_code in ('1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20')
+    group by `time_period.time_period_code` 
+    order by `time_period.time_period_code`; """
+query_order_store_avg = client.query_sql(sql_order_store_avg)
+
+# 取堂食、外卖占比 & 营业额、利润排名
+sql_proport_total = """
+    select y.store_name,x.ts_data,x.wm_data from
+(select a.store_code,sum(a.ts/a.total) as ts_data,sum(b.wm/b.total2) as wm_data from 
+(select store_code,SUM(`marketing_channel.quantity`) as ts,SUM(`time_period.quantity`) as total 
+FROM cdp.sd_trade_byday array join marketing_channel,time_period
+group by store_code
+having `marketing_channel.marketing_channel_name` ='堂食' ) a
+LEFT JOIN
+(select store_code,SUM(`marketing_channel.quantity`) as wm,sum(`time_period.quantity`) as total2
+FROM cdp.sd_trade_byday array join marketing_channel,time_period
+group by store_code
+having `marketing_channel.marketing_channel_name` ='外卖' ) b
+on a.store_code=b.store_code
+group BY a.store_code) x
+INNER JOIN 
+(select DISTINCT store_code,store_name from cdp.bo_store) y
+on x.store_code=y.store_code
+where store_name != ''
+"""
+query_proport_total = client.query_sql(sql_proport_total)
+
+sql_profit_amount_order = """
+    select store_name,sum(trade_amount_avg) as amount,sum(net_profit_total) as profit 
+    from cdp.sd_trade_bymonth 
+    group by store_name 
+    order by sum(trade_amount_avg) DESC,sum(net_profit_total) DESC """
+query_profit_amount_order = client.query_sql(sql_profit_amount_order)
+query_profit_amount_order_rank = [list(i) for i in query_profit_amount_order]
+a = query_profit_amount_order_rank
+# 进行排序，提取下标
+# for i in a:
+#     print(i)
+# print('排序后：-----------')
+
+# 先对第一列进行排序
+
+# 中括号内填写按哪行排序
+list_data = sorted(a, key=lambda x: (x[1]))
+# 倒序输出
+return_list = []
+index = 1  # 自定义下标
+for row in reversed(list_data):
+    # print(row[0],row[1],row[2],index)
+    return_list.append([row[0], row[1], row[2], index])
+    index = index + 1
+
+# print(return_list)
+
+# 第一列数据排序处理完毕，再对第二列进行排序
+# 中括号内填写按哪行排序
+list_data_2 = sorted(return_list, key=lambda x: (x[2]))
+# 倒序输出
+return_list_2 = []
+index2 = 1  # 自定义下标
+for row in reversed(list_data_2):
+    # print(row[0], row[1], row[2], row[3],index2)
+    return_list_2.append([row[0], row[1], row[2], row[3], index2])
+    index2 = index2 + 1
+# print(return_list_2)
+
+# 转换成dataframe类型
+query_time_order = [list(i) for i in time_order]
+query_store_count = [list(i) for i in query_store_count]
+query_all_store_avg = [list(i) for i in query_all_store_avg]
+query_order_store_avg = [list(i) for i in query_order_store_avg]
+query_store_merge = [list(i) for i in query_store_merge]
+query_order_data = [list(i) for i in query_order_data]
+query_proport_total = [list(i) for i in query_proport_total]
+
+df_time_order = DataFrame(query_time_order)
+df_store_count = DataFrame(query_store_count, dtype=int)
+df_all_store_avg = DataFrame(query_all_store_avg)
+df_order_store_avg = DataFrame(query_order_store_avg)
+df_store_merge = DataFrame(query_store_merge)
+df_order_data = DataFrame(query_order_data)
+df_proport_total = DataFrame(query_proport_total)
+df_rank = DataFrame(return_list_2)
+
+# 门店数量数据加工
+def demo(data):
+    res = []
+    for i in range(6): # 整体数据分成了6份
+        pd_data = data[i*4:(i+1)*4]
+        demo_list = list()
+        print(333,pd_data,5555)
+        for j in range(4):  # 把每份数据往后加4次
+            demo_list.append(pd_data.loc[j+(i*4)]['1_x'])
+            demo_list.append(pd_data.loc[j+(i*4)]['1_y'])
+        res.append(demo_list)
+    pd_data = DataFrame(res)
+    return pd_data
+# 门店数量数据
+data_time_storecount = df_time_order.merge(df_store_count, left_on=0, right_on=0)
+res= demo(data_time_storecount)
+# print(11,res,222)
+# 所有门店时段内平均销售额
+data_time_allstore_avg = df_time_order.merge(df_all_store_avg, left_on=0, right_on=0)
+# (1)合并门店表和月营业排名表 and (2)继续合并时段平均销售额表
+data_month_order = df_time_order.merge(df_order_store_avg, left_on=0, right_on=0)
+# 合并销售占比
+data_sales_proport = df_rank.merge(df_proport_total, left_on=0, right_on=0)
