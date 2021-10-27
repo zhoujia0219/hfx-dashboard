1f35ccf1eb473dd054ffc7dea8d641eb9dd2395e
guang <guangfeng.jiang@ruipos.com>
2021-10-27 17:06:47 +0800

    导出功能

--- conf/basic_const.py
+++ conf/basic_const.py
@@ -11,14 +11,28 @@ REAL_TIME_TOTAL_SALE_INTERVAL_TIME = 30 * 1000  # 实时页面左侧销售总额
 
 # 数据导出导入的表名和字段名
 IMPORT_EXPORT_TABLENAME_FIELD = {  # 导入和导出的数据配置
-    "fyjh": [
-        "data_transfer",  # 对应的数据表名
-        ['column_1', 'column_2', 'column_3', 'column_4', 'column_5', 'column_6', 'column_7', 'column_8'],  # 数据库字段
-        ["字段1", '字段2', '字段3', '字段4', '字段5', '字段6', '字段7', '字段8']  # excel表格数据中的表头
+    'PayMode': [  # 支付方式
+        'cdim_pay_mode',  # 对应的数据表名
+        ['pay_mode_code', 'pay_mode_name'],  # 数据库字段
+        ['支付方式编码', '支付方式名称']  # excel表格数据中的表头
     ],
-    'brand': [
-        'pdim_brand',  # 对应的数据表名
-        ['brand_code', 'std_brand_code', 'db_schema', 'brand_name', 'trade_code', 'company_code', 'version_time'],
-        ["品牌编码", '标准品牌编码（随主数据）', '数据库或schema名称', '品牌名称', '行业编码', '公司编码', '版本编码']  # excel表格数据中的表头
-    ]
+
+    'payChannel': [  # 支付渠道
+        'cdim_pay_channel',  # 对应的数据表名
+        ['pay_channel_code', 'pay_channel_name'],  # 数据库字段
+        ['支付渠道编码', '支付渠道名称']  # excel表格数据中的表头
+    ],
+
+    'Charge': [  # 费用
+        '',  # 对应的数据表名
+        [],  # 数据库字段
+        []  # excel表格数据中的表头
+    ],
+
+    'CustDistrict': [  # 定义城市区划
+        '',  # 对应的数据表名
+        [],  # 数据库字段
+        []  # excel表格数据中的表头
+    ],
+
 }
